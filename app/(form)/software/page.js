5f9159510a54df5b716e4047484a693e5c0ab1ce
akari39 <xiaofan2.rs@icloud.com>
2025-04-11 18:01:15 +1000

    Added DataGrid with a template data.

--- app/(form)/software/page.js
+++ app/(form)/software/page.js
@@ -2,8 +2,9 @@
 
 import FilterSearchBar from "@/app/components/FilterSearchBar";
 import SingleChoiceChipFilter from "@/app/components/SingleChoiceChipFilter";
-import { Stack } from "@mui/material";
-import { useState } from "react";
+import { Link, Stack } from "@mui/material";
+import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
+import { useMemo, useState } from "react";
 
 const SOFTWARE_USING = '正在使用';
 const SOFTWARE_ALL = '全部';
@@ -20,6 +21,43 @@ export default function Software() {
         [`${SOFTWARE_USING}`]: { selected: true }, // default choice
         [`${SOFTWARE_ALL}`]: { selected: false }
     });
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: 25,
+    });
+
+    const columns = useMemo(() => [
+        { field: 'license_id', headerName: '授权ID', flex: 1 },
+        { field: 'name', headerName: '软件名称', flex: 2 },
+        { field: 'status', headerName: '授权状态', flex: 1 },
+        { field: 'expiration_time', headerName: '过期时间', flex: 2 },
+        {
+            field: 'action',
+            type: 'actions',
+            headerName: '操作',
+            flex: 1,
+            getActions: (params) => [
+                <GridActionsCellItem
+                    icon={<Link>查看详情</Link>}
+                    label="查看详情"
+                    onClick={() => { }}
+                    showInMenu={false}
+                    disableRipple
+                />
+            ],
+        },
+    ]);
+
+    const rows = [
+        {
+            id: 1,
+            license_id: 1,
+            name: 'windows',
+            status: '已领取',
+            expiration_time: '2025-10-24 12:00',
+            action: 1,
+        },
+    ];
 
     return (
         <Stack direction="column" >
@@ -28,6 +66,21 @@ export default function Software() {
                 options={SOFTWARE_SEARCH_OPTIONS}
                 default={SOFTWARE_DEFALUT_SEARCH_OPTIONS}
                 placeholder="搜索软件" />
+            <DataGrid
+                columns={columns}
+                slots={{ toolbar: GridToolbar }}
+                paginationModel={paginationModel}
+                onPaginationModelChange={(model) => setPaginationModel(model)}
+                rowCount={rows.length}
+                paginationMode="server"
+                rows={rows}
+                sx={{
+                    marginLeft: "32px",
+                    marginRight: "32px",
+                    marginTop: "8px",
+                    marginBottom: "8px",
+                }}
+            />
         </Stack>
     );
 }
