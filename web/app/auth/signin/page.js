edb5f9453ff7219d9fb6607a7df691f4323823ca
akari39 <xiaofan2.rs@icloud.com>
2025-04-29 18:36:15 +1000

    Signin page localised in Chinese.

--- web/app/auth/signin/page.js
+++ web/app/auth/signin/page.js
@@ -7,6 +7,24 @@ export default function SignIn() {
     return (
         <SignInPage
             providers={providerMap}
+            localeText={{
+                signInTitle: '登录',
+                signInSubtitle: '请输入邮箱和密码继续',
+                oauthSignInTitle: '使用第三方账号登录',
+                magicLinkSignInTitle: 'Magic Link 登录',
+                passkeySignInTitle: 'Passkey 登录',
+                signInRememberMe: '记住我',
+                email: '邮箱地址',
+                password: '密码',
+
+                or: '或',
+                with: '使用',
+                cancel: '取消',
+                ok: '确定',
+                save: '保存',
+                close: '关闭',
+                to: '到',
+            }}
             signIn={async (
                 provider,
                 formData,
